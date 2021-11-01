69171b81d25a505c3d281fcdf06013aec1e2bec5
tc <1510298763@qq.com>
2021-11-01 10:56:42 +0800

    swagger

--- src/main/java/com/bonjour/demo/test/common/config/SwaggerConfig.java
+++ src/main/java/com/bonjour/demo/test/common/config/SwaggerConfig.java
@@ -39,8 +39,6 @@ public class SwaggerConfig {
                 // 扫描所有有注解的api，用这种方式更灵活
                 .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 // 扫描指定包中的swagger注解
-                // .apis(RequestHandlerSelectors.basePackage("com.ylzinfo.sxzt"))
-                // 扫描所有 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
                 /* 设置安全模式，swagger可以设置访问token */
